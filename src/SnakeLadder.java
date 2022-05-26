0e545f45b998fd503b6151fa40028f722c8dfcf2
Avani14 <trivediavani14@gmail.com>
2022-05-26 20:29:30 +0530

    Snake and ladder option

--- src/SnakeLadder.java
+++ src/SnakeLadder.java
@@ -1,7 +1,8 @@
 class RollTheDice{
+	public static byte result;
 	public static byte rollDice()
 	{
-		byte result = (byte)(Math.random()*10+1);
+		result = (byte)(Math.random()*10+1);
 		if(result>6)
 		{
 			result%=2;
@@ -10,10 +11,37 @@ class RollTheDice{
 	return result;
 	}
 }
+class CheckOption{
+	static public byte positionOf1;
+	public static void checkOpt()
+	{
+		int opt = (int)(Math.random()*10);
+		switch (opt)
+		{
+		case 0:
+			System.out.println("The option was snake");
+			positionOf1-=RollTheDice.result;
+			if(positionOf1<0) {positionOf1=0;}
+			System.out.println("Current position of player : "+positionOf1);
+			break;
+		case 1:
+			System.out.println("The option was ladder");
+			positionOf1+=RollTheDice.result;
+			System.out.println("Current position of player : "+positionOf1);
+			break;
+		default:
+			System.out.println("The option was no play");
+			System.out.println("Current position of player : "+positionOf1);
+		}
+	}
+}
 public class SnakeLadder {
-	static byte positionOf1;
+	
 	public static void main(String[] args) {
-		System.out.println("Current position of player : "+positionOf1);
+		System.out.println("Current position of player : "+CheckOption.positionOf1);
 		System.out.println("You got ... "+RollTheDice.rollDice());
+		while(CheckOption.positionOf1<100) {
+		CheckOption.checkOpt();
+		}
 	}
 }
