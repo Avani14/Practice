b21481f7ccb196fe4c5faf27abfc346b215f819a
Avani14 <trivediavani14@gmail.com>
2022-05-26 19:50:03 +0530

    Roll the dice

--- src/SnakeLadder.java
+++ src/SnakeLadder.java
@@ -1,7 +1,19 @@
+class RollTheDice{
+	public static byte rollDice()
+	{
+		byte result = (byte)(Math.random()*10+1);
+		if(result>6)
+		{
+			result%=2;
+		}
 
+	return result;
+	}
+}
 public class SnakeLadder {
 	static byte positionOf1;
 	public static void main(String[] args) {
 		System.out.println("Current position of player : "+positionOf1);
+		System.out.println("You got ... "+RollTheDice.rollDice());
 	}
 }
